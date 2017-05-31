419d166e8dbadc0bc32722dbec3e087b0526cf86
felkost <felkost@ukr.net>
2017-05-31 12:28:02 +0300

    створено .gitignore

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h1>Hello</h1>
+<?php
+  echo "Привіт, Світ!";
+  $dat1 = date("d.m.y");
+  echo "$dat1";
+  $dat1 = date("h:m:s");
+  echo "$dat1";
+?>
+</body>
+</html>

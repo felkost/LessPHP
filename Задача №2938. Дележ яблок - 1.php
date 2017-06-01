eb25b110c3b8ae3e133470cbd9676a4b97623f42
felkost <felkost@ukr.net>
2017-06-01 12:44:25 +0300

    Код правильни: виконується правильно. На сайті олімпіади проблема з функціями для цілочисельного ділення

--- Задача №2938. Дележ яблок - 1.php
+++ Задача №2938. Дележ яблок - 1.php
@@ -1,6 +1,7 @@
 /**
 * http://informatics.mccme.ru/mod/statements/view3.php?id=2296&chapterid=2938#1
 * https://olympiads.ru/mosolymp/2011-12/tour1/io.shtml
+* http://php.net/manual/ru/function.intdiv.php
 */
 <?php
 # Считываем весь файл при помощи функции file_get_contents,
@@ -13,7 +14,7 @@ $InputData=preg_split('/\s+/', trim(file_get_contents('input.txt')));
 # Теперь в элементах массива $InputData[0] и $InputData[1]
 # записаны два входных числа.
 # Запишем их сумму в переменную $Answer
-$Answer=$InputData[0]+$InputData[1];
+$Answer=intdiv($InputData[1],$InputData[0]);
 
 # Объявляем файл FOUT для вывода данных
 $FOUT=fopen("output.txt","w");

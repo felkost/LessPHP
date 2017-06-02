4ed80886616da638bf5a473adfe767afdb797b67
felkost <felkost@ukr.net>
2017-06-02 15:05:33 +0300

    ЧВиведення тексту ( 2 рядки) та чисел. Поєднання їх за допомогою "."

--- Задача №2937. Следующее и предыдущее.php
+++ Задача №2937. Следующее и предыдущее.php
@@ -1,15 +1,23 @@
 /**
  * http://informatics.mccme.ru/mod/statements/view3.php?id=2296&chapterid=2937#1
+
+http://addphp.ru/materials/base/1_12.php
  */
 <?php
-
 $InputData=preg_split('/\s+/', trim(file_get_contents('input.txt')));
 
-$Answer=(integer)($InputData[1]/$InputData[0]); 
+$numb= $InputData[0];
+
+$nub_next = $numb+1;
+$nub_pred = $numb-1;
+
+$Answer1="The next number for the number ".$numb." is ".$nub_next.".";
+$Answer2="The previous number for the number ".$numb." is ".$nub_pred.".";
+
 
 $FOUT=fopen("output.txt","w");
 
-fputs($FOUT,$Answer);
+fputs($FOUT,$Answer1."\n".$Answer2);
 
 fclose($FOUT);
 ?>

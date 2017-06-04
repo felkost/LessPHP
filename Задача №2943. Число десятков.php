16c67b7616ee9a8b063e5d3637d0b5112e00e3e0
fel kost <felkost@ukr.net>
2017-06-04 21:22:38 +0300

    Задача для перевірки способів цілочисельного ділення та читання даних: все описано в "Корисних спостереженнях"

---
+++ Задача №2943. Число десятков.php
@@ -0,0 +1,22 @@
+/**
+ * http://informatics.mccme.ru/mod/statements/view3.php?id=2296&chapterid=2943#1
+ *
+ */
+<?php
+$InputData=preg_split('/\s+/', trim(file_get_contents('input.txt')));
+
+$numb= $InputData[0];
+
+#$numb=fgets(STDIN);
+
+$digit = $numb /10 % 10;
+
+
+#$FOUT=fopen("output.txt","w");
+
+printf("%d\n", $digit);
+
+#fputs($FOUT,$Answer."\n");
+
+#fclose($FOUT);
+?>

6f83a3ae2c38856a43b5c8fd8b6d9b156a56a85c
felkost <felkost@ukr.net>
2017-06-07 14:33:22 +0300

    На роботы добавлено PHPUnit для php-7.1. Вдома потрыбно добавити для iншої  версії php Відео в Довідникую Добавити папку в ігнор

---
+++ Array/Array2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PascalABC: Array1. Дано целое число N (> 0).
+ * Сформировать и вывести целочисленный массив размера N, содержащий N первых положительных нечетных чисел:
+ * 1, 3, 5, … .
+ */
+
+//$InputData=preg_split('/\s+/', trim(file_get_contents('input.txt')));
+//
+//$n = $InputData[0];
+//$arr[]=1;
+//for($i=1; $i<=$n; ++$i)
+//    $arr[$i] = $arr[$i-1] + 2;
+//for($i=0; $i<count($arr); ++$i)
+//    printf("%d\t",$arr[$i]);
+
+
+?>
